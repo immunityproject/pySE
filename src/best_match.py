ef9d0dece73e8a3ce98602aae772be1096f609c8
salt <none@none.com>
2017-07-06 00:04:47 -0400

    comments on matcher script

--- src/best_match.py
+++ src/best_match.py
@@ -1,5 +1,10 @@
-from proteins import all_aminos,chains,chain_bases
+#!/usr/bin/python
+# coding=utf-8
+#
+# Searches proteins for specific epitope occurrences and outputs the chain relative location.
+# Can fuzzy match partially mutated epitopes, or sections with missing data.
 
+from proteins import all_aminos,chains,chain_bases
 from argparse import ArgumentParser
 
 
@@ -15,6 +20,10 @@ class Result:
 	
 
 def getScore(seq1, seq2):
+	"""Compare two sequences of equal length to determine if they are a match.
+	:param seq1: the first amino sequence to compare.
+	:param seq2: the second amino sequence to compare.
+	:returns: a number representing the similarity between sequences."""
 	
 	total_match = 0.0
 
@@ -27,12 +36,14 @@ def getScore(seq1, seq2):
 
 	return total_match/len(seq1)
 
-def scanSingleProteinForMatch(protein,proteinArray, epitope, cutoff):
-	#print "Scanning protein " + protein
+def scanSingleProteinForMatch(protein, proteinArray, epitope, cutoff):
+	"""Scan a protein for an epitope.
+	:param protein: the name of the protein to scan.
+	:param proteinArray: protein/chain aminos for each protein to search.
+	:param epitope: the amino sequence to scan for.
+	:param cutoff: minimal confidence score to include results.
+	:returns: array of Result objects for any qualifying matches"""
 	resultArray = []
-
-	#def __init__(self, proteinName, offset, sequence, score):
-	#where we are, stop before len(epitope) so we don't go off the end of the amino array)
 
 	for chain in proteinArray[protein]:
 		chain_base = chain_bases[protein][chain] 
@@ -49,6 +60,11 @@ def scanSingleProteinForMatch(protein,proteinArray, epitope, cutoff):
 	
 
 def scanProteinsForMatch(proteinArray, epitope, cutoff):
+	"""Scan all proteins for an epitope.
+	:param proteinArray: protein/chain aminos for each protein to search.
+	:param epitope: the amino sequence to scan for.
+	:param cutoff: minimal confidence score to include results.
+	:returns: sorted array of Result objects for any qualifying matches"""
 	
 	resultsArray = []
 
@@ -62,13 +78,17 @@ def scanProteinsForMatch(proteinArray, epitope, cutoff):
 
 
 def printTopResults(resultsArray, eptiope, numberOfResults):
+	"""Print details for the best matches for a given epitope, ranked by confidence.
+	:param resultsArray: list of all result objects from a previous search step, usually from running scanProteinsForMatch
+	:param epitope: the amino sequence that was searched for.
+	:param numberOfResults: the number of results to show, max.
+	:returns: None."""
 	print "Best " + str(numberOfResults) + " matches for:" + eptiope
 	
 	for i in range(numberOfResults):
 		if(i >= len(resultsArray)):
 			print str(i) + ": ---"
 		else:
-			#def __init__(self, proteinName, offset, sequence, score):
 			result = resultsArray[i]
 			print str(i) + ": "  + result.proteinName + "-" + result.chain  + "-" + str(result.offset),
 			print " (" + result.sequence +  ") " + "SCORE:" + str(result.score)  
@@ -76,12 +96,20 @@ def printTopResults(resultsArray, eptiope, numberOfResults):
 
 
 def findChainsFromBestHit(bestResult):
+	""" Helper function that returns the full set of chains for a high confidence hit on a single chain on a given protein."""
 	for chain in chains[bestResult.proteinName]:
 		if bestResult.chain in chain:
 			return chain
 
 
 def constructResultDict(first,last,chain_array,conf,ref):
+	"""Collect variables relevant to a epitope match to a dictionary.
+	:param first: the starting position of the match in the matching chain aminos.
+	:param last: the ending position of the match in the matching chain aminos.
+	:param chain_array: an array containing the set of chains upon which this match was found.
+	:param conf: how confident the match was.
+	:param ref: a reference value that we expect, such as a published result.
+	:returns: a dictionary containing these values with keys used in other scripts."""
 	return_dict = {}
 	return_dict['first']=first
 	return_dict['last']=last
@@ -92,7 +120,9 @@ def constructResultDict(first,last,chain_array,conf,ref):
 	return return_dict
 
 def findLocalMatchFromList(epitope_array):
-
+	"""Scans all proteins for a list of epitopes.
+	:param epitope_array: array of amino sequences that should be seeked.
+	:returns: a dictionary of proteins, each protein a dictionary of epitopes that were found there, each with relevant epitope match data.  Can be inserted into proteins.py file to be permanently referenced by other scripts."""
 	#initialize protein dict.		
 	protein_result = {};
 	for protein in all_aminos:
@@ -106,7 +136,8 @@ def findLocalMatchFromList(epitope_array):
 		#use best result and look up chain data.
 		bestResult = resultsArray[0]
 		chain_array = findChainsFromBestHit(bestResult)
-		
+	
+		#if the confidence is less than 2/3, assume site is missing from data or too mutated to recognize.	
 		if bestResult.score < .665:
 			print "Bad conf for: " + shortname + " " + str(bestResult.score)
 
@@ -114,10 +145,8 @@ def findLocalMatchFromList(epitope_array):
 			protein_result[bestResult.proteinName][shortname] = constructResultDict(bestResult.offset, bestResult.offset+len(bestResult.sequence)-1, chain_array, bestResult.score, epitope_data[1] )
 
 	print protein_result
+	return protein_result
 
-	#for protein in protein_result:
-	#	for epitope in protein_result[protein]:
-	#		print protein + "\t" + str(protein_result[protein][epitope]["chains"])
 		
 
 if __name__ == "__main__":
@@ -149,10 +178,6 @@ if __name__ == "__main__":
 			epitope_array.append([split_line[0],split_line[1]])
 
 		findLocalMatchFromList(epitope_array)
-	
 			
-		#except Exception,e:		
-		#	print "Error opening " + args.filename
-		#	print str(e)
 	else:
 		print "Use -e or -f to specify epitope sequence to search for!"
