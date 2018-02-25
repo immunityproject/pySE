aea183b40c68cc9ab01c91a67b6943db6810b72a
Rick Carback <carback1@lexumo.com>
2018-02-25 21:59:08 +0000

    Add a create_foldx_jobs tool based on generate_buildmodel.py

---
+++ pyse/create_foldx_jobs.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+
+"""
+Create Foldx Jobs
+
+This tool creates foldx job directories to be parsed and processed by
+scan_foldx_jobs. It takes as input a source pdb file, which is parsed
+to generate all possible combinations of mutations based on the ATOM
+lines read from the file.
+
+The jobs created by this tool are used to run the foldx BuildModel
+command.  The job directories contain 2 data elements:
+  1. list.txt - the name of the source pdb file. This is usually a repaired
+                pdb file.
+  2. individual_list.txt - wild type amino (starting amino letter), the chain
+                           (A, B, C, ...), the residue number (site position),
+                           and the mutant residue (target mutation).
+
+See the BuildModel command in the foldx documentation for more information.
+
+We generate a separate job directory for every site position and
+target mutation, grouped by the chains with matching wild types in
+that position. For example, if site 2 of a given pdb has the following
+wild types and chains:
+  A. LYS (K)
+  B. LYS (K)
+  C. GLU (E)
+  D. HIS (H)
+  E. GLU (E)
+
+This will produce 3 sets: {A, B}, {C, E}, and {D}. Each set will produce a
+mutation against each target, including itself.
+
+The original version of this tool can be found in generate_buildmodel.py
+
+"""
+from __future__ import print_function
+
+import click
+import os
+import uuid
+
+from collections import defaultdict
+
+from pdb import parse_pdb
+from proteins import codes
+
+
+def makeJobDir(pdbfile, jobid, line, basedir='.'):
+    """Generate a job directory for a work item.
+    :param pdbfile: the name of the pdbfile that this work item is used with.
+    :param line: the mutation(s) that this work item should perform.
+    :returns: None.  Generates a folder and FoldX work files on disk. """
+    pdb_name = pdbfile
+    dir_name = "foldxbm-{}".format(jobid)
+    full_loc = os.path.join(basedir, protein_name, dir_name)
+    os.makedirs(full_loc)
+    with open(os.path.join(full_loc, 'individual_list.txt'), 'wb') as il:
+        il.write(os.path.basename(pdbfile) + '\n')
+    with open(os.path.join(full_loc, 'list.txt'), 'wb') as l:
+        l.write(line + '\n')
+
+def generate_chaingroups(pdbfn):
+    """This parses the pdb then generates the following structure for
+    generating build job folders:
+      site: {wildtype: [groups]}
+    """
+    chaingroups = defaultdict(defaultdict(set))
+    with open(pdbfn, 'r') as pdbfile:
+        for pdbentry in parse_pdb(pdbfile):
+            wildtype = pdbentry['remnant']
+            chain = pdbentry['chain']
+            site = pdbentry['position']
+            chaingroups[site][wildtype].add(chain)
+    return chaingroups
+
+@click.command()
+@click.option('--outdir', '-o', default='.',
+              help='The output job directory')
+@click.option('--protein', '-p', default=None,
+              help=('The name of the protein (jobs are put in '
+                    'jobdir/proteinname/jobname)'))
+@click.argument('pdbfile')
+def main(outdir, protein, pdbfile):
+    outdir = os.path.join(outdir, protein)
+    print('Reading {} and putting job directories in {}'.format(pdbfile,
+                                                                outdir))
+    chaingroups = generate_chaingroups(pdbfile)
+    for site, chaingroup in chaingroups.items():
+        for wildtype, chains in chaingroup:
+            wtamino = codes[wildtype]
+            for mut in codes.values():
+                mutlist = ','.join(['{}{}{}{}'.format(wtamino, chain, site, mut)
+                                    for chain in chains]) + ';'
+                jobid = '{}-{}-{}'.format(site, wtamino, mut)
+                makeJobDir(pdbfile, jobid, mutlist, outdir)
